d3acc9ade20ade851c7abfb95151d69e57f23ae7
Björn Orri Guðmundsson <bjorng@tempo.io>
2019-11-06 12:40:24 +0000

    added put, query and delete examples

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -4,48 +4,54 @@ import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.amazonaws.services.dynamodbv2.document.DynamoDB;
 import com.amazonaws.services.dynamodbv2.document.Item;
+import com.amazonaws.services.dynamodbv2.document.ItemCollection;
 import com.amazonaws.services.dynamodbv2.document.PutItemOutcome;
+import com.amazonaws.services.dynamodbv2.document.QueryOutcome;
+import com.amazonaws.services.dynamodbv2.document.ScanOutcome;
 import com.amazonaws.services.dynamodbv2.document.Table;
+import com.amazonaws.services.dynamodbv2.document.spec.QuerySpec;
+import com.amazonaws.services.dynamodbv2.document.spec.ScanSpec;
+import com.amazonaws.services.dynamodbv2.document.utils.NameMap;
+import com.amazonaws.services.dynamodbv2.document.utils.ValueMap;
 import com.amazonaws.services.dynamodbv2.model.AttributeDefinition;
 import com.amazonaws.services.dynamodbv2.model.KeySchemaElement;
 import com.amazonaws.services.dynamodbv2.model.KeyType;
 import com.amazonaws.services.dynamodbv2.model.ProvisionedThroughput;
 import com.amazonaws.services.dynamodbv2.model.ScalarAttributeType;
-import com.google.gson.Gson;
 
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 
 public class App
 {
-    public static void main( String[] args ) {
-        Gson gson = new Gson();
-        System.out.println(gson.toJson("Ivar my friend, here is a working example"));
-        createTable();
-        getAllTables();
-        putStuff();
-    }
 
-    public static void getAllTables() {
+    private static final String MOVIES = "Movies";
+
+    public static void main(String[] args ) {
         AmazonDynamoDB client = getClient();
         DynamoDB dynamoDB = new DynamoDB(client);
+        createTable(dynamoDB);
+        getAllTables(dynamoDB);
+        putStuff(dynamoDB);
+        scanStuff(dynamoDB);
+        queryStuff(dynamoDB);
+        deleteStuff(dynamoDB);
+    }
+
+    private static void getAllTables(DynamoDB dynamoDB) {
         var tables = dynamoDB.listTables();
         for (Table table : tables) {
             System.out.println(table.getTableName());
         }
     }
 
-    public static void createTable() {
-        AmazonDynamoDB client = getClient();
-
-        DynamoDB dynamoDB = new DynamoDB(client);
-
-        String tableName = "Movies";
+    private static void createTable(DynamoDB dynamoDB) {
 
         try {
             System.out.println("Attempting to create table; please wait...");
-            Table table = dynamoDB.createTable(tableName,
+            Table table = dynamoDB.createTable(MOVIES,
                     Arrays.asList(new KeySchemaElement("year", KeyType.HASH), // Partition
                             // key
                             new KeySchemaElement("title", KeyType.RANGE)), // Sort key
@@ -63,15 +69,13 @@ public class App
 
     }
 
-    public static void putStuff() {
-        AmazonDynamoDB client = getClient();
-        DynamoDB dynamoDB = new DynamoDB(client);
-        Table table = dynamoDB.getTable("Movies");
+    private static void putStuff(DynamoDB dynamoDB) {
+        Table table = dynamoDB.getTable(MOVIES);
 
         int year = 2015;
         String title = "The Big New Movie";
 
-        final Map<String, Object> infoMap = new HashMap<String, Object>();
+        final Map<String, Object> infoMap = new HashMap<>();
         infoMap.put("plot", "Nothing happens at all.");
         infoMap.put("rating", 0);
 
@@ -85,6 +89,77 @@ public class App
         }
         catch (Exception e) {
             System.err.println("Unable to add item: " + year + " " + title);
+            System.err.println(e.getMessage());
+        }
+    }
+
+    private static void scanStuff(DynamoDB dynamoDB) {
+
+        Table table = dynamoDB.getTable(MOVIES);
+
+        ScanSpec scanSpec = new ScanSpec().withProjectionExpression("#yr, title, info.rating")
+                .withFilterExpression("#yr between :start_yr and :end_yr").withNameMap(new NameMap().with("#yr", "year"))
+                .withValueMap(new ValueMap().withNumber(":start_yr", 1950).withNumber(":end_yr", 1959));
+
+        try {
+            ItemCollection<ScanOutcome> items = table.scan(scanSpec);
+
+            for (Item item : items) {
+                System.out.println(item.toString());
+            }
+
+        }
+        catch (Exception e) {
+            System.err.println("Unable to scan the table:");
+            System.err.println(e.getMessage());
+        }
+    }
+
+    private static void queryStuff(DynamoDB dynamoDB) {
+        Table table = dynamoDB.getTable(MOVIES);
+
+        HashMap<String, String> nameMap = new HashMap<>();
+        nameMap.put("#yr", "year");
+
+        HashMap<String, Object> valueMap = new HashMap<>();
+        valueMap.put(":yyyy", 2015);
+
+        QuerySpec querySpec = new QuerySpec().withKeyConditionExpression("#yr = :yyyy").withNameMap(nameMap)
+                .withValueMap(valueMap);
+
+        ItemCollection<QueryOutcome> items;
+        Iterator<Item> iterator;
+        Item item;
+
+        try {
+            System.out.println("Movies from 2015");
+            items = table.query(querySpec);
+            iterator = items.iterator();
+            while (iterator.hasNext()) {
+                item = iterator.next();
+                System.out.println(item.getNumber("year") + ": " + item.getString("title"));
+            }
+
+        }
+        catch (Exception e) {
+            System.err.println("Unable to query movies from 2015");
+            System.err.println(e.getMessage());
+        }
+
+    }
+
+    private static void deleteStuff(DynamoDB dynamoDB) {
+        Table table = dynamoDB.getTable(MOVIES);
+
+        try {
+            System.out.println("Attempting to delete table; please wait...");
+            table.delete();
+            table.waitForDelete();
+            System.out.print("Success.");
+
+        }
+        catch (Exception e) {
+            System.err.println("Unable to delete table: ");
             System.err.println(e.getMessage());
         }
     }
